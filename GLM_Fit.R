4490d2f6762af950fdd9a6744cb9876e2ce34057
John R Foster <fosterj@bu.edu>
2019-03-28 13:38:49 -0400

    first pass at fitting GLM to mozzie data with monthly weather

---
+++ GLM_Fit.R
@@ -0,0 +1,79 @@
+
+#' Function for the random walk model. Observations are modelled under a Poisson, while the two species are 
+#' modelled with multivariate normal process error as most counties are correlated
+#' 
+#' @param county.name Name of county of interest
+#' @param spp species, one of "albo" or "aegypti"
+#' @param aedes.data mozzie data
+#' @param met.data monthly weather data
+#' @param met.driver vector of met variables to use in model. Any of "prcp", "tmin", "tmax", "RH"
+#' @param n.iter number of iterations, default = 5000
+#' @param inits initial conditions, default = NULL
+
+GLM_Fit <- function(county.name, spp, aedes.data, met.data, met.driver, inits = NULL, ...){
+  
+  # get county of interest and create a "year-month" column
+  county.sub <- aedes.data %>% 
+    filter(state_county == county.name) %>% 
+    unite("year_month", year, month, sep = "-")
+  
+  # aggregate counts for each month, as they are separated by trap type
+  y.albo <- aggregate(county.sub$num_albopictus_collected, by = list(county.sub$year_month), FUN = sum)[,2]
+  y.aegypti <- aggregate(county.sub$num_aegypti_collected, by = list(county.sub$year_month), FUN = sum)[,2]
+  
+  # get met data for county and variables of interest
+  met.sub <- met.data[[county.name]][,met.driver] 
+
+  # convert to met.sub to matrix and add column of 1's for intercept
+  MET <- as.matrix(cbind(rep(1, nrow(met.sub)), met.sub))
+  
+  # use appropriate data (which species?)
+  if(spp == "albo"){
+    y <- y.albo
+  } else {
+    y <- y.aegypti
+  }
+  
+  # beta means - for prior
+  b0 <- rep(0, ncol(MET))
+  
+  # beta precisions - for prior
+  Vb <- solve(diag(10000, ncol(MET)))
+  
+  # create data list for JAGS
+  data <- list(y = y,
+               n.month = length(y),
+               MET = MET,
+               b0 = b0,
+               Vb = Vb)
+  
+  model <- "
+  model{
+    
+    #### Data Model
+    for(i in 1:n.month){
+      y[i] ~ dpois(x[i])
+    }
+    
+    #### Process Model
+    for(i in 2:n.month){
+      mu[i] <- x[i-1] + MET[i,] %*% beta 
+      x[i] ~ dnorm(mu[i], tau_proc)
+    }
+    
+    #### Priors
+    x[1] ~ dpois(5)
+    beta ~ dmnorm(b0, Vb)
+    tau_proc ~ dgamma(0.01,0.01)
+  
+  }"
+  
+  j.model <- jags.model(file = textConnection(model),
+                        data = data,
+                        n.chains = 3,
+                        inits = inits,
+                        ...)
+  
+  return(j.model)
+  
+}
