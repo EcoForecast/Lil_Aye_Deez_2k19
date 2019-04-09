6c67328a5e75ede8cdbcb9881345ac75dfd79635
John R Foster <fosterj@bu.edu>
2019-04-09 16:40:09 -0400

    change function to calcalculate burnin and thin output

--- Basic_Diagnostics_Function.R
+++ Basic_Diagnostics_Function.R
@@ -4,23 +4,35 @@ library(rjags)
 
 # inputs are the model parameter data and model predictive data
 
-diagnostics <- function(model_out_params, model_out_pred, burnin){
+diagnostics <- function(model_out_params, model_out_pred, n.iter){
   
   # convergence diagnostics
   gelman.diag(model_out_params)
   GBR <- gelman.plot(model_out_params)
   
-  #remove burnin
-  out.burn.params <- window(model_out_params,start=burnin)
-  out.burn.pred <- window(model_out_pred, start=burnin)
+  burnin <- GBR$last.iter[tail(which(apply(GBR$shrink[,,2] > 1.1, 1, any)),1)+1]
+  if(length(burnin) == 0){burnin = 1}
   
+  total.iter <- dim(model_out_params[[1]])[1]
+  burn.iter <- total.iter - burnin
+  
+  #remove burnin
+  out.burn.params <- window(model_out_params,
+                            start = burnin,
+                            thin = floor(burn.iter/n.iter),
+                            end = total.iter)
+  
+  out.burn.pred <- window(model_out_pred,
+                          start = burnin,
+                          thin = floor(burn.iter/n.iter),
+                          end = total.iter)
   
   # convergence diagnostics after burnin
-  gelman.diag(out.burn.params)
-  GBR <- gelman.plot(out.burn.params)
+  #gelman.diag(out.burn.params)
+  #GBR <- gelman.plot(out.burn.params)
   
   #plot model parameters
-  plot(out.burn.params)
+  #plot(out.burn.params)
   
   out.burn <- list()
   out.burn[[1]] = out.burn.params
